4043fabe7494af8c0cbf92678c8aebfb987490eb
Alicia MARCHAND <amarchan@e3r8p21.clusters.42paris.fr>
2022-10-12 14:11:28 +0200

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,24 +6,30 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 09:51:38 by amarchan          #+#    #+#             */
-/*   Updated: 2022/10/12 10:00:40 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/10/12 11:18:44 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring>
+#include <stdio.h>
 
+// Static_cast is a safer cast than the implicit C style cast. If you try to
+// cast an entity which is not compatible to another, then static_cast gives
+// you an compilation time error unlike the implicit c-style cast.
 int main(int argc, char *argv[]) {
 	int	i;
-	int	j;
 
 	i = 1;
-	if (!argc)
-		return (0);
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	while (argv[i])
 	{
-		for (int j = 0; j < strlen(argv[i]); j++)
-			putchar(toupper(argv[j]);
-		
+		std::string s(argv[i]);
+		for (size_t j = 0; j < strlen(argv[i]); j++)
+			std::cout << static_cast<char>(std::toupper(s[j]));
+		std::cout << std::endl;
+		i++;
 	}
 	return (0);
 }
